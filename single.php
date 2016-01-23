aa7345d0a8bfd986d309347516c7fa9ffb481c84
Mike Neumegen <mike@cloudcannon.com>
2016-01-23 12:54:32 -0800

    removed sidebar

--- single.php
+++ single.php
@@ -15,16 +15,10 @@ while ( have_posts() ) : the_post();
 
 	get_template_part( 'template-parts/content', get_post_format() );
 
-	// If comments are open or we have at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) :
-		comments_template();
-	endif;
-
 endwhile; // End of the loop.
 ?>
 
 
 
 <?php
-get_sidebar();
 get_footer();
